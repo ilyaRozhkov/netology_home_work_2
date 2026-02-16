2d4c8de6558b518c2dbea91cb472ebebb3700815
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-16 18:49:22 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,14 @@
+//﻿#include <iostream>
+//#include <clocale>
+
+#include <iostream>;
+#include <clocale>;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian"); // Для корректного отображения кириллицы  
+	std::string userWord;
+	std::cout << "Введите слово::\n";
+	std::cin >> userWord;
+	std::cout << "Вы ввели: " << "\n" << userWord << "\n";
+}

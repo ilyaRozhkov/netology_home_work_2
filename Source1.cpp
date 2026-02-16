83f54f9e02c503892033e597ce67a3ec8fdf80c4
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-16 19:51:27 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -8,7 +8,8 @@
 int main() {
 	setlocale(LC_ALL, "Russian"); // Для корректного отображения кириллицы  
 	std::string userWord;
-	std::cout << "Введите слово::\n";
+	std::cout << "Введите слово:\n";
 	std::cin >> userWord;
 	std::cout << "Вы ввели: " << "\n" << userWord << "\n";
+
 }

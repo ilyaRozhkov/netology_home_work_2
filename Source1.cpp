db7e422b2f91965e9bef0d10227e30429d93f8ef
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-16 19:56:02 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,12 +1,13 @@
-//﻿#include <iostream>
-//#include <clocale>
-
-#include <iostream>;
-#include <clocale>;
+#include <iostream>
+#include <clocale>
+#include <windows.h>
 
 
 int main() {
 	setlocale(LC_ALL, "Russian"); // Для корректного отображения кириллицы  
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
 	std::string userWord;
 	std::cout << "Введите слово:\n";
 	std::cin >> userWord;

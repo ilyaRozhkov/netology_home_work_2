81ed1dba8ae2104c8a8c2356a4fc442176b6b041
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-16 18:44:51 +0300

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,14 @@
+﻿//﻿#include <iostream>
+//#include <clocale>
+
+#include <iostream>;
+#include <clocale>;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian"); // Для корректного отображения кириллицы  
+	int userNumber;
+	std::cout << "Введите число:\n";
+	std::cin >> userNumber;
+	std::cout <<"Вы ввели: " << "\n" << userNumber << "\n";
+}

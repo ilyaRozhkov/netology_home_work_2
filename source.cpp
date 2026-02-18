97800b1ea9f958a603ede14103c37490d19e677a
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-18 09:38:43 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,9 +1,6 @@
-﻿//﻿#include <iostream>
-//#include <clocale>
-
-#include <iostream>;
-#include <clocale>;
-
+#include <iostream>
+#include <clocale>
+#include <string>
 
 int main() {
 	setlocale(LC_ALL, "Russian"); // Для корректного отображения кириллицы  
@@ -11,4 +8,5 @@ int main() {
 	std::cout << "Введите число:\n";
 	std::cin >> userNumber;
 	std::cout <<"Вы ввели: " << "\n" << userNumber << "\n";
+
 }
